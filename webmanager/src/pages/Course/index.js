3c117b10af7714d7c5c8674e0d417d068bd7d9b5
junfeng <lijunran.ljr@alibaba-inc.com>
2022-03-17 18:19:22 +0800

    feat: 新增课程管理pop框

--- webmanager/src/pages/Course/index.js
+++ webmanager/src/pages/Course/index.js
@@ -1,83 +1,164 @@
-import React, { memo } from 'react'
-import { Table, Button } from "antd";
-import { CourseWrapper } from './style';
+import React, { memo, useState } from "react";
+import { Table, Button, Modal } from "antd";
+import { CourseWrapper } from "./style";
 
 const Course = memo(() => {
   const columns = [
     {
-      title: '课程名称',
-      key: 'name',
-      dataIndex: 'name',
-      render: text => <a onClick={(e) => e.defaultPrevented()}>{text}</a>,
+      title: "课程名称",
+      key: "name",
+      dataIndex: "name",
+      render: (text) => <a onClick={(e) => e.defaultPrevented()}>{text}</a>,
     },
     {
-      title: '课程号',
-      key: 'curriculumId',
-      dataIndex: 'curriculumId',
+      title: "课程号",
+      key: "curriculumId",
+      dataIndex: "curriculumId",
     },
     {
-      title: '上课时间',
-      key: 'curriculumTime',
-      dataIndex: 'curriculumTime',
+      title: "上课时间",
+      key: "curriculumTime",
+      dataIndex: "curriculumTime",
     },
     {
-      title: '上课教室',
-      key: 'classroom',
-      dataIndex: 'classroom',
+      title: "上课教室",
+      key: "classroom",
+      dataIndex: "classroom",
     },
     {
-      title: '学生人数',
-      key: 'studentNumber',
-      dataIndex: 'studentNumber',
+      title: "学生人数",
+      key: "studentNumber",
+      dataIndex: "studentNumber",
     },
     {
-      title: '操作',
-      key: 'action',
-      dataIndex: 'action',
+      title: "操作",
+      key: "action",
+      dataIndex: "action",
       render: (text, record) => (
         <>
-          <Button type="primary" className='btn'>修改</Button>
-          <Button type="primary" danger>删除</Button>
+          <Button type="primary" className="btn" onClick={() => showModal(2)}>
+            修改
+          </Button>
+          <Button type="primary" className="btn">
+            导出成绩
+          </Button>
+          <Button type="primary" danger onClick={() => showModal(3)}>
+            删除
+          </Button>
         </>
       ),
     },
   ];
-  
+
   const data = [
     {
-      name: '软件开发环境1-3班',
+      name: "软件开发环境1-3班",
       curriculumId: 81420,
       curriculumTime: "9-16周",
-      classroom: '致用106',
+      classroom: "致用106",
       studentNumber: "106人",
     },
     {
-      name: '软件开发环境1-3班',
+      name: "软件开发环境1-3班",
       curriculumId: 81420,
       curriculumTime: "9-16周",
-      classroom: '致用106',
+      classroom: "致用106",
       studentNumber: "106人",
     },
     {
-      name: '软件开发环境1-3班',
+      name: "软件开发环境1-3班",
       curriculumId: 81420,
       curriculumTime: "9-16周",
-      classroom: '致用106',
+      classroom: "致用106",
       studentNumber: "106人",
     },
     {
-      name: '软件开发环境1-3班',
+      name: "软件开发环境1-3班",
       curriculumId: 81420,
       curriculumTime: "9-16周",
-      classroom: '致用106',
+      classroom: "致用106",
       studentNumber: "106人",
     },
   ];
+
+  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+  const [isAddModalVisible, setIsAddModalVisible] = useState(false);
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+
+  const showModal = (type) => {
+    console.log(1)
+    switch (type) {
+      case 1:
+        setIsAddModalVisible(true);
+        break;
+      case 2:
+        setIsEditModalVisible(true);
+        break;
+      case 3:
+        setIsDeleteModalVisible(true);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleOk = (type) => {
+    switch (type) {
+      case 1:
+        setIsAddModalVisible(false);
+        break;
+      case 2:
+        setIsEditModalVisible(false);
+        break;
+      case 3:
+        setIsDeleteModalVisible(false);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleCancel = (type) => {
+    switch (type) {
+      case 1:
+        setIsAddModalVisible(false);
+        break;
+      case 2:
+        setIsEditModalVisible(false);
+        break;
+      case 3:
+        setIsDeleteModalVisible(false);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <CourseWrapper>
+      <div className="actionWrapper">
+        <Button type="primary" style={{ marginBottom: "10px" }} onClick={() => showModal(1)}>
+          新增课程
+        </Button>
+      </div>
       <Table columns={columns} dataSource={data} />
+      <Modal title="新增课程" visible={isAddModalVisible} onOk={() => handleOk(1)} onCancel={() => handleCancel(1)}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      <Modal title="编辑课程" visible={isEditModalVisible} onOk={() => handleOk(2)} onCancel={() => handleCancel(2)}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      <Modal title="删除课程" visible={isDeleteModalVisible} onOk={() => handleOk(3)} onCancel={() => handleCancel(3)}>
+        <p>你确定要删除该课程吗？这会导致该课程的所有学生信息删除</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </CourseWrapper>
-  )
-})
+  );
+});
 
-export default Course
+export default Course;
